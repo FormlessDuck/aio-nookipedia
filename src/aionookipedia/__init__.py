ba51f93f29ade4965f4a90e21df6fb776ff28bf3
FormlessDuck <42917343+FormlessDuck@users.noreply.github.com>
2025-11-18 15:05:05 -0800

    updated version

--- src/aionookipedia/__init__.py
+++ src/aionookipedia/__init__.py
@@ -4,5 +4,5 @@ __title__ = "aio-nookipedia"
 __author__ = "FormlessDuck"
 __license__ = "WTFPL"
 __copyright__ = "Copyright 2025-present formlessduck"
-__version__ = "0.1.0"
+__version__ = "0.1.9"
 
